bdd8899837872e54f38c6d3386bdde3a6fac3b38
zhangguangpeng <zhangguangpemng503@126.com>
2017-11-01 16:46:48 +0800

    修改项目结构

---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WDComponent
+//
+//  Created by zhangguangpeng on 2017/11/1.
+//  Copyright © 2017年 zhangguangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WDComponent
+//
+//  Created by zhangguangpeng on 2017/11/1.
+//  Copyright © 2017年 zhangguangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
